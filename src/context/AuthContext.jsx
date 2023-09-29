995151e2c00f298b9ddc6f42135307510e31ee6f
Syed Hisham Akmal <hisham0502@gmail.com>
2023-09-29 23:21:32 +0530

    ✅  context/AuthContext.jsx in place

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,74 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth, db } from "../firebase/firebase";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import {doc, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+
+export const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const navigate=useNavigate()
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Sign up function
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  //for admins
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth,email,password)
+  }
+  
+
+  function addUserToFirestore(uid, userData) {
+    try {
+      // Reference to the 'users' collection
+      return setDoc(doc(db, "users", uid), userData);
+    } catch (error) {
+      console.error('Error adding user to Firestore:', error);
+    }
+  }
+
+  // Log out function
+  function logout() {
+    signOut(auth);
+    toast.success('Logged out!')
+    navigate('/login')
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout,
+    addUserToFirestore,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
