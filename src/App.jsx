93358b93bacd2c1923fca7f0314e517fae66bbc4
Syed Hisham Akmal <hisham0502@gmail.com>
2023-09-29 23:29:40 +0530

    ✅ Added sub directories and relevant files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth"
-import { db } from "./firebase/firebase"
+import { useAuth } from "./context/AuthContext" 
 
 export default function App() {
+
+  const { currentUser } = useAuth()
+
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
