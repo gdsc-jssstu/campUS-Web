c1ff9a2a0aa90bca6f52fab277a31e25dea3b369
Syed Hisham Akmal <hisham0502@gmail.com>
2023-09-29 23:34:28 +0530

    ✅ Basic components generated along(dummy for now) along with routing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
 import { useAuth } from "./context/AuthContext" 
+import Signup from "./pages/Signup"
+import Profile from "./pages/Profile"
+import { ToastContainer } from "react-toastify"
+import Login from "./pages/Login"
+import Home from "./pages/Home"
 
 export default function App() {
 
   const { currentUser } = useAuth()
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Navbar />
+    <Routes>
+      {!currentUser && <Route  path="/signup" element={<Signup />} />}
+      {!currentUser && <Route  path="/signup" element={<Login />} />}
+      {currentUser && <Route  path="/profile" element={<Profile />} />}
+      {!currentUser && <Route  path="/profile" element={<Home />} />}
+    </Routes>
+    <ToastContainer position="top-right"/>
+  </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Router>
     <AuthProvider >
     <App />
     </AuthProvider >
-  </React.StrictMode>,
+  </Router>
+  
 )
