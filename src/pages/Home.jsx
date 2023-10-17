bac43ee017d22dd917875b16fda7c6105f9c6648
Sumit Kumar Jha <jhasumit4742@gmail.com>
2023-10-17 14:58:46 +0530

    Create responsive Home.jsx with tailwind css as per the provided figma design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import WelcomeImage from '../assets/heroImage.svg'
+import FeatureImage from '../assets/featureImage.svg'
+import DownArrow from '../assets/DownTwistedArrow.svg'
+import RightArrow from '../assets/SmallRightArrow.svg'
+import { useNavigate } from "react-router-dom";
 
-function Home() {
+
+
+const Home = () => {
+    const navigate = useNavigate();
   return (
-    <div>Home</div>
+    <section id="homePageWrapper">
+        <div id="heroSection" className= 'flex flex-col lg:flex-row md:flex-row sm:flex-row  justify-between pt-20 pl-20 pr-40 pb-2'>
+            <div id="welcomeTitle" className='pt-9 pb-2 text-6xl text-san-marino font-playfair-display '>
+                <h1>Hi! <br />
+                    Welcome <br />
+                    To <br />
+                    CampUs <br />
+                </h1>
+            </div>
+            <div id="welcomeImage" className='w-0 h-0 lg:h-100 lg:w-auto sm:h-100 sm:w-auto md:h-100 md:w-auto lg:shrink-0 md:shrink-0 sm:shrink-0'>
+                <img src={WelcomeImage} alt="Welcome"/>
+            </div>
+        </div>
+
+        <div id="featureSection" className='flex flex-col lg:flex-row p-2 sm:flex-col md:flex-col '>
+            <img src={FeatureImage} alt="Feature" className='w-full'/>
+            <div id="featureDescription" className='pl-4 pt-24 sm:pt-24 text-san-marino font-playfair-display font-thin'>
+                <h1 className='text-5xl'>Features</h1>
+                <p className='mt-2 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tenetur consectetur! Expedita aut doloremque sit, nulla nisi dignissimos eius minima sed. Laudantium, consequuntur facere ipsam beatae id veniam natus, asperiores quisquam, quam aliquid atque fugit reprehenderit inventore. Eum soluta repudiandae dolorum necessitatibus! Blanditiis veritatis harum, rerum, nihil ipsa vero eum quos dolor at vel atque mollitia unde quas inventore enim quis, culpa illo corrupti nam. Soluta similique, culpa inventore est voluptates laudantium fuga corrupti quasi minima ipsam sequi nam ratione expedita molestias, asperiores dolorum commodi? Ducimus nostrum alias accusamus repellat fugit quo distinctio rem mollitia dolores nisi, blanditiis, eligendi numquam repellendus illum id laboriosam similique non ab iusto, asperiores cupiditate labore quibusdam nobis. Inventore soluta a accusamus dolore earum minus tempore, excepturi sed commodi, dignissimos quasi officiis assumenda recusandae est adipisci sunt quae doloremque non deserunt porro, reprehenderit dolor aut perferendis! Eius modi vel voluptatibus? Numquam aspernatur cum nesciunt cupiditate, dolor est qui sit quasi velit! Porro adipisci, harum quas enim tempora aut quis quos sed consequuntur illo pariatur. Soluta voluptates laudantium amet omnis eveniet officia quae accusantium itaque temporibus tempora! Amet magnam, odit quam, beatae aperiam quas assumenda et itaque illo esse impedit sint quasi dolore ipsam iusto placeat molestiae quaerat, autem inventore ex. Sit provident dolore, corrupti nobis atque accusamus nostrum vel et. Optio qui dicta suscipit, at vero tempore temporibus obcaecati laborum impedit voluptatibus quod repellat deleniti ex rerum maiores architecto explicabo quam odit ipsam rem! Modi deleniti eaque eos corporis adipisci amet incidunt odio quisquam distinctio?</p>
+            </div>
+        </div>
+
+
+        <div id="getStartedSection" className='pb-2 flex justify-center'>
+            <img src={DownArrow} alt="Twisted downwards arrow" />
+            <div className='pl-2 pt-60'>
+                <button onClick={() => navigate("/login")}
+                className=' border bg-lime-green text-3xl gap-2 m-2 p-4 rounded-lg flex text-san-marino font-playfair-display h-24 sm:h-20 md:h-20 lg:h-20 '>Get Started <img src={RightArrow} alt="right arrow"/> </button>
+            </div>
+        </div>
+
+    </section>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,23 @@ export default {
       "san-marino": "#4a55a2",
       "tea-green": "#bfedc1",
       "white": "#fff",
+      "yellow-tick": "#efead8",
+      "lime-green":"#C8ECC5",
     },
     extend: {
       spacing: {
         "62px": "62px",
         "78px": "78px",
         "120px": "120px",
-      }
+      },
+      padding:{
+        "350": "350px",
+        "500": "500px",
+        "700": "700px",
+      },
+      margin:{
+        "200": "-200px",
+      },
     },
     fontFamily: {
       "playfair-display": ["Playfair Display"],
