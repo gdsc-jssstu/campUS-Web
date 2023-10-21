c45853178f87f2c36e34aafdb7308967430ee974
Yasani Samara Simha Reddy <101248119+samara6855@users.noreply.github.com>
2023-10-21 19:00:10 +0530

    Simplified App Routing in App.jsx
    Made the routing in the App component clearer and more organized. Now, it's easier to see which routes are for signed-in users and which ones are for visitors. Removed repeated code for a cleaner look.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+// Components and Pages
 import Navbar from "./components/Navbar";
-import { useAuth } from "./context/AuthContext";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
-import { ToastContainer } from "react-toastify";
 import AboutUs from "./pages/AboutUs";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Requests from "./pages/Requests"
 import MyRequests from "./pages/MyRequests"
 
+// Context
+import { useAuth } from "./context/AuthContext";
+
 export default function App() {
   const { currentUser, setCurrentUser } = useAuth();
 
-  const pathComponent = {
-    requests: <Requests />,
-    myrequests: <MyRequests />,
-    profile: <Profile />,
-    signup: <Signup />,
-    login: <Login />,
-  };
+  const privateRoutes = [
+    { path: 'requests', component: <Requests /> },
+    { path: 'myrequests', component: <MyRequests /> },
+    { path: 'profile', component: <Profile /> },
+  ];
+
+  const authRoutes = [
+    { path: 'signup', component: <Signup /> },
+    { path: 'login', component: <Login /> },
+  ];
 
   function PrivateRoute({ children }) {
-    return currentUser ? <>{children}</> : <Navigate to="/" />
+    return currentUser ? <>{children}</> : <Navigate to="/" />;
   }
 
   function AuthenticateRoute({ children }) {
-    return currentUser ? <Navigate to="/profile" /> : <>{children}</>
+    return currentUser ? <Navigate to="/profile" /> : <>{children}</>;
   }
 
   return (
@@ -35,33 +42,22 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
-        {
-          ["requests", "myrequests", "profile"].map((path, index) => (
-            <Route
-              key={index}
-              path={`/${path}`}
-              element={
-                <PrivateRoute>
-                  {pathComponent[path]}
-                </PrivateRoute>
-              }
-            />
-          ))
-        }
 
-        {
-          ["signup", "login"].map((path, index) => (
-            <Route
-              key={index}
-              path={`/${path}`}
-              element={
-                <AuthenticateRoute>
-                  {pathComponent[path]}
-                </AuthenticateRoute>
-              }
-            />
-          ))
-        }
+        {privateRoutes.map(({ path, component }, index) => (
+          <Route
+            key={index}
+            path={`/${path}`}
+            element={<PrivateRoute>{component}</PrivateRoute>}
+          />
+        ))}
+
+        {authRoutes.map(({ path, component }, index) => (
+          <Route
+            key={index}
+            path={`/${path}`}
+            element={<AuthenticateRoute>{component}</AuthenticateRoute>}
+          />
+        ))}
       </Routes>
       <ToastContainer position="top-right" />
     </>
