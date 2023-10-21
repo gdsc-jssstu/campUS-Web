e87a576e0b9f520f64976b73d59a245d361c0784
Eduardo Gris <gris.eduardo@gmail.com>
2023-10-21 09:44:55 -0300

    Enhance routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,67 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { useAuth } from "./context/AuthContext";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
 import { ToastContainer } from "react-toastify";
+import AboutUs from "./pages/AboutUs";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Requests from "./pages/Requests"
+import MyRequests from "./pages/MyRequests"
 
 export default function App() {
   const { currentUser, setCurrentUser } = useAuth();
+
+  const pathComponent = {
+    requests: <Requests />,
+    myrequests: <MyRequests />,
+    profile: <Profile />,
+    signup: <Signup />,
+    login: <Login />,
+  };
+
+  function PrivateRoute({ children }) {
+    return currentUser ? <>{children}</> : <Navigate to="/" />
+  }
+
+  function AuthenticateRoute({ children }) {
+    return currentUser ? <Navigate to="/profile" /> : <>{children}</>
+  }
 
   return (
     <>
       <Navbar currentUser={currentUser} />
       <Routes>
         <Route path="/" element={<Home />} />
-        {!currentUser && <Route path="/signup" element={<Signup />} />}
-        {!currentUser && <Route path="/login" element={<Login />} />}
-        {currentUser && <Route path="/profile" element={<Profile />} />}
+        <Route path="/aboutus" element={<AboutUs />} />
+        {
+          ["requests", "myrequests", "profile"].map((path, index) => (
+            <Route
+              key={index}
+              path={`/${path}`}
+              element={
+                <PrivateRoute>
+                  {pathComponent[path]}
+                </PrivateRoute>
+              }
+            />
+          ))
+        }
+
+        {
+          ["signup", "login"].map((path, index) => (
+            <Route
+              key={index}
+              path={`/${path}`}
+              element={
+                <AuthenticateRoute>
+                  {pathComponent[path]}
+                </AuthenticateRoute>
+              }
+            />
+          ))
+        }
       </Routes>
       <ToastContainer position="top-right" />
     </>
